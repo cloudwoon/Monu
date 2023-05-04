1e4a9e682c94631d0bb6728da8c698b618a2c289
cloudwoon <cloud.wooo0n@gmail.com>
2023-05-05 01:05:28 +0900

    메인 페이지 개발중

--- app/src/main/java/kr/co/woon/monu/MainActivity.kt
+++ app/src/main/java/kr/co/woon/monu/MainActivity.kt
@@ -9,10 +9,17 @@ import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
@@ -22,6 +29,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -29,10 +37,13 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 
 class MainActivity : ComponentActivity() {
@@ -41,17 +52,6 @@ class MainActivity : ComponentActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
 
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                ObjectAnimator.ofFloat(splashScreenView, View.ALPHA, 1f, 0f).run {
-                    interpolator = AnticipateInterpolator()
-                    duration = 200L
-                    doOnEnd { splashScreenView.remove() }
-                    start()
-                }
-            }
-        }
 
         setContent {
             Scaffold(
@@ -67,7 +67,7 @@ class MainActivity : ComponentActivity() {
                 Box(
                     modifier = Modifier.fillMaxSize(),
                 ) {
-
+                    statusBar()
                 }
 
             }
@@ -98,20 +98,76 @@ fun topAppBar() {
 
 @Composable
 fun bottomNavigationBar() {
-    var selectedItem by remember { mutableStateOf(0) }
+    /*var selectedItem by remember { mutableStateOf(0) }
     val items = remember {
         listOf("집중력향상법", "홈", "설정")
-    }
-    BottomNavigation(
-        backgroundColor = Color(0xFFF3F4FF),
+    }*/
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(70.dp)
     ) {
-        items.forEachIndexed { index, item ->
-            BottomNavigationItem(
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = null) },
-                label = { Text(item) },
-                selected = selectedItem == index,
-                onClick = { selectedItem = index }
-            )
+        Row(modifier = Modifier.fillMaxSize().padding(top = 10.dp).background(color = Color(0xFFF3F4FF))) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(0.5f)
+                    .fillMaxHeight()
+                    .background(color = Color(0xFFF3F4FF)),
+                contentAlignment = Alignment.Center
+            ) {
+                Column() {
+                    Image(
+                        painter = painterResource(id = R.drawable.btn_bottom_nv_1),
+                        contentDescription = "집중력 향상 방법",
+                        modifier = Modifier
+                            .size(120.dp)
+                            .padding(end = 20.dp)
+                    )
+                }
+
+
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth(1f)
+                    .fillMaxHeight()
+                    .background(color = Color(0xFFF3F4FF)),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.btn_bottom_nv_3),
+                    contentDescription = "설정",
+                    modifier = Modifier
+                        .size(48.dp)
+                        .padding(start = 20.dp)
+                )
+
+            }
         }
+
+        Image(
+            painter = painterResource(id = R.drawable.btn_bottom_nv_2),
+            contentDescription = "홈"
+        )
+
+    }
+}
+
+@Composable
+fun statusBar() {
+    val systemUiController = rememberSystemUiController()
+    val useDarkIcons = !isSystemInDarkTheme()
+
+    DisposableEffect(systemUiController, useDarkIcons) {
+        // Update all of the system bar colors to be transparent, and use
+        // dark icons if we're in light theme
+        systemUiController.setSystemBarsColor(
+            color = Color(0xFFF3F4FF),
+            darkIcons = useDarkIcons
+        )
+
+        // setStatusBarColor() and setNavigationBarColor() also exist
+
+        onDispose {}
     }
 }
